099f073c7801bf188b65a5f6d9bba48bf98ebd2b
raju197 <rajukumarmandal1997@gmail.com>
2019-06-11 11:34:02 +0530

    changes

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    { public static int add(int a , int b) {
+
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(add(10 , 20);
+            Console.ReadLine();
+        }
+    }
+}
